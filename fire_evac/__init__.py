b9c9d175d59df599801195d9f51d12a732bd1ed6
Khanh Tran <vktran@stanford.edu>
2024-11-11 22:20:24 -0800

    Debugged map generation with city feature

---
+++ fire_evac/__init__.py
@@ -0,0 +1,9 @@
+from gymnasium import register
+
+# Not sure if this is the correct way to register the environment as a gymnasium environment
+
+register(
+    id="code/fireEvac-v0",
+    entry_point="code:WildfireEvacuationEnv",
+    max_episode_steps=200,
+)
