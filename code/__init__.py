5756bae4544bcfb962ec586d8410b67d5f143de7
maiapec <155457758+maiapec@users.noreply.github.com>
2024-11-11 18:33:26 -0800

    small change

--- code/__init__.py
+++ code/__init__.py
@@ -3,7 +3,7 @@ from gymnasium import register
 # Not sure if this is the correct way to register the environment as a gymnasium environment
 
 register(
-    id="fireEvac/PyroRL-v0",
+    id="code/fireEvac-v0",
     entry_point="code:WildfireEvacuationEnv",
     max_episode_steps=200,
 )
