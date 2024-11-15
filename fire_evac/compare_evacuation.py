885de39153a698ff6d7490cc6c8809ff9777887e
maiapec <155457758+maiapec@users.noreply.github.com>
2024-11-14 19:41:03 -0800

    added ideas for validation

---
+++ fire_evac/compare_evacuation.py
@@ -0,0 +1,15 @@
+""" 
+Ideas for validating and comaaring evacuation models. 
+
+1. Basic models to compare to:
+        - random evacuation
+        - greedy evacuation
+        - parametrized rule-based evacuation (to define)
+
+2. Experiments to validate behaviors of models:
+        - In a very simple grid, with a single fire cell, the agent should move away from the fire cell.
+
+3. Metrics to compare models:
+        - Average final reward over multiple runs (for a given map?)
+        - ?
+"""
